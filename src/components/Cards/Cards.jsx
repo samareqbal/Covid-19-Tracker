01822961f31a6f132940122e6dbbb3aef01dbe36
Samar Eqbal <samareqbal17@gmail.com>
2020-06-24 13:05:18 +0530

    changes to animation time

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -16,7 +16,7 @@ const Cards = ({data:{confirmed , recovered , deaths , lastUpdate}}) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
-                            <CountUp start={0} end={confirmed.value} duration={2.5} separator="," />
+                            <CountUp start={0} end={confirmed.value} duration={1.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
@@ -26,7 +26,7 @@ const Cards = ({data:{confirmed , recovered , deaths , lastUpdate}}) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5">
-                            <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
+                            <CountUp start={0} end={recovered.value} duration={1.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of recoveries from COVID-19</Typography>
@@ -36,7 +36,7 @@ const Cards = ({data:{confirmed , recovered , deaths , lastUpdate}}) => {
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5">
-                            <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
+                            <CountUp start={0} end={deaths.value} duration={1.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
